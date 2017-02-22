99d26ff25a808d6800d4904f9646f87ea30afd98
Fabiola Ampuero <fabi.ampuero.chaclco@gmail.com>
2017-02-22 11:01:06 -0500

    se agrego jquery

--- js/main.js
+++ js/main.js
@@ -6,8 +6,8 @@ function validateForm(){
     correoText();
     passwordText();
     selectValid();
-    name.addEventListener("keyup",isMayus(name));
-    lastName.addEventListener("keyup",isMayus(lastName));
+    name.on("keyup", isMayus);
+    lastName.on("keyup", isMayus);
 }
 
 function haveNumbers (_value){
@@ -23,37 +23,40 @@ function createSpan (_value,_menssage){
     _value.parent().append('<span>'+ _menssage +'</span>');
 }
 function removeSpan (_value){
-    _value.parent().remove(_value.nextSibling);
+    _value.next().remove();
 }
 
-function isMayus (_value){
-    var array=_value.val().split("");
-    var first=array[0];
-    var upper=first.toUpperCase();
-    var space = false;
-    
-    for(var i=1; i<array.length; i++) {
-            if(space){
-                upper += array[i].toUpperCase();
-                space = false;
-            } else {
-                upper += array[i].toLowerCase();
-                if(array[i] == " ")
-                    space = true;
-            }
-        _value.val() = upper;
+
+function isMayus (){
+    if(this.value.length != 0){
+        var array = this.value.split("");
+        var first = array[0];
+        var upper = first.toUpperCase();
+        var space = false;
+        for(var i=1; i<array.length; i++) {
+                if(space){
+                    upper += array[i].toUpperCase();
+                    space = false;
+                } else {
+                    upper += array[i].toLowerCase();
+                    if(array[i] == " ")
+                        space = true;
+                }
+            this.value= upper;
+        }
     }
 }
+
 
 function nombreText (){
 	var name=$("#name");
     
-    if(name.next() == null){
+    if(name.next().length == 0){
         if(isComplete(name.val()) || haveNumbers(name.val())){
             createSpan(name,"Nombre inválido");
         }       
     } else{
-        if(name.next() != null){
+        if(name.next().length != 0){
             if(!isComplete(name.val()) && !haveNumbers(name.val())){
                 removeSpan(name);
             }
@@ -63,12 +66,12 @@ function nombreText (){
 
 function apellidoText (){
 	var lastName=$("#lastname");
-    if(lastName.next() == null){
+    if(lastName.next().length == 0){
         if(isComplete(lastName.val()) || haveNumbers(lastName.val())){
             createSpan(lastName,"Apellido inválido");
         }
     } else{
-        if(lastName.next()!= null){
+        if(lastName.next().length != 0){
             if(!isComplete(lastName.val()) && !haveNumbers(lastName.val())){
                 removeSpan(lastName);
             }
@@ -79,12 +82,12 @@ function apellidoText (){
 function correoText (){
     var regex= /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/;
     var correo=$("#input-email");
-    if(correo.next() == null){
+    if(correo.next().length == 0){
         if(isComplete(correo.val()) || regex.test(correo.val())==false){
             createSpan(correo,"Correo inválido");
         }
     } else{
-        if(correo.next() != null){
+        if(correo.next().length != 0){
             if(!isComplete(correo.val()) && regex.test(correo.val())==true){
                 removeSpan(correo);
             }
@@ -94,12 +97,12 @@ function correoText (){
 
 function passwordText (){
     var password=$("#input-password");
-    if(password.next()==null){
+    if(password.next().length==0){
         if(password.val()=="123456" || password.val()=="098754" || password.val().length<6){
             createSpan(password,"Esta Contraseña no es válida");
         }
     } else{
-        if(password.next() != null){
+        if(password.next().length != 0){
             if(password.val().length>=6 && password.val()!="123456" && password.val()!="098754"){
                 removeSpan(password);
             }
@@ -109,12 +112,12 @@ function passwordText (){
 
 function selectValid (){
     var select=$("#select");
-    if(select.next() == null){
+    if(select.next().length == 0){
         if(select.val()==0){
             createSpan(select,"Seleccione una bicicleta");
         }       
     } else{
-        if(select.next() != null){
+        if(select.next().length != 0){
             if(select.val() != 0){
                 select.parent().removeChild(select.nextSibling);
             }
